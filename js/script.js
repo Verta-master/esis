9a16a80cbc26344d85c315555d216cc13b8e4e65
Verta-master <vertyagina@gmail.com>
2021-11-08 17:01:13 +0600

    Mobile

--- js/script.js
+++ js/script.js
@@ -14,9 +14,16 @@ $('.search__open').click(function() {
   $('.search__drop').slideToggle();
 });
 
+//Mobile footer
 if (window.innerWidth < 766) {
   $('.footer__title').click(function() {
     $(this).next().slideToggle();
     $(this).toggleClass('footer__title--up');
   })
 }
+
+//City choice
+$('.header__arrow').click(function() {
+  $('.header__list').slideToggle();
+  $(this).toggleClass('header__arrow--up');
+})
