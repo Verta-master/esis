7e71e060bbe772183c8fc297595cfeca1d7ab2b8
Verta-master <vertyagina@gmail.com>
2021-12-27 20:43:56 +0600

    edits

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ $('.header__arrow').click(function() {
   $('.header__list').slideToggle();
   $(this).toggleClass('header__arrow--up');
 })
+
+//Scroll to top
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $(".btn-top").fadeIn();
+  } else {
+    $(".btn-top").fadeOut();
+  };
+});
+
+$(".btn-top").click(function () {
+  $("body,html").animate({
+    scrollTop: 0
+  }, 400);
+  return false;
+});
