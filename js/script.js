2e1b4393538c0b12878adebe4f01b67d14ded03b
Verta-master <vertyagina@gmail.com>
2021-12-28 10:31:54 +0600

    Update 2021-12-28T04:31:49.059Z

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 //Mobile menu
-$('.header__btn').click(function() {
-  $('.menu').addClass('menu--opened');
-  $('.menu').slideDown();
-});
-
-$('.menu__close').click(function() {
-  $('.menu').removeClass('menu--opened');
-  $('.menu').slideUp();
-})
+//$('.header__btn').click(function() {
+//  $('.menu').addClass('menu--opened');
+//  $('.menu').slideDown();
+//});
+//
+//$('.menu__close').click(function() {
+//  $('.menu').removeClass('menu--opened');
+//  $('.menu').slideUp();
+//});
 
 // Mobile search
 $('.search__open').click(function() {
@@ -27,3 +27,19 @@ $('.header__arrow').click(function() {
   $('.header__list').slideToggle();
   $(this).toggleClass('header__arrow--up');
 })
+
+//Scroll to top
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $(".btn-top").fadeIn();
+  } else {
+    $(".btn-top").fadeOut();
+  };
+});
+
+$(".btn-top").click(function () {
+  $("body,html").animate({
+    scrollTop: 0
+  }, 400);
+  return false;
+});
